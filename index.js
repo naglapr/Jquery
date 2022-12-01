7b4f8a96aa88bfb298060333058bdcdf155a779e
naglapr <naglapr@rknec.edu>
2022-12-01 21:28:32 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*$(document).ready(function(){
+    var ele=$("h1").css("color","red");
+    console.log(ele);
+});*/
+
+document.querySelector("button").addEventListener("click", function(){
+    
+    let dice1=Math.floor(Math.random()*6)+1;
+    let dice2=Math.floor(Math.random()*6)+1;
+    
+    document.querySelector("#dice1").src="images/dice"+dice1+".png";
+    document.querySelector("#dice2").setAttribute("src","images/dice"+dice2+".png");
+
+    let x=document.querySelector("h1");
+    console.log(x);
+
+    if(dice1 > dice2){
+        document.querySelector("h1").innerText="Player 1 Wins";
+    }else if(dice1 < dice2){
+        document.querySelector("h1").innerText="Player 2 Wins";
+    }else{
+        document.querySelector("h1").innerText="DRAW";
+    } 
+});
+
